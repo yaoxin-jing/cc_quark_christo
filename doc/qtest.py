f7a18c13ce7162205cc7d4f5d9bb37243b682ff6
ch3lt <christob.tr@hotmail.com>
2025-04-09 19:52:57 +0200

    test: performance: initial automation: use csv format for startup test

--- doc/qtest.py
+++ doc/qtest.py
@@ -20,18 +20,20 @@ def log(_name, path):
     end_dir = path.joinpath(_name)
     if end_dir.exists() == False:
         os.makedirs(end_dir.as_posix())
-    file_name = end_dir.joinpath(ctime)
-    with open(file_name, 'x') as f:
-        f.write("Test:"+_name+",")
+    file_name = end_dir.joinpath(ctime + '.csv')
     return file_name
 
 def startup_time(log_name, runtimes=["quark"], tries=10):
     rtime = ""
-    for r in runtimes:
-        if r != 'native':
-            rtime = "--runtime="+r
-        command = f'date +%s%N;docker run --rm {rtime} ubuntu:20.04 /bin/date +%s%N'
-        with open(log_name, 'a') as f:
+    tmp_file = log_name.as_posix() + '.tmp'
+    result = subprocess.run("docker create ubuntu:20.04", shell=True, stdout=subprocess.PIPE,
+                                        stderr=subprocess.STDOUT, text=True)
+    cid = result.stdout.split('\n')[0]
+    with open(tmp_file, 'a+') as f:
+        for r in runtimes:
+            if r != 'native':
+                rtime = "--runtime="+r
+            command = f'date +%s%N;docker run --rm {rtime} ubuntu:20.04 /bin/date +%s%N'
             f.write(TRY % (r, tries))
             f.write(SEPARATE)
             for i in range(tries):
@@ -40,6 +42,9 @@ def startup_time(log_name, runtimes=["quark"], tries=10):
                 vals = result.stdout.split('\n')[:-1]
                 diff = (float(vals[1]) - float(vals[0])) * 10**(-9)
                 f.write("%.9f\n" % diff)
+    _adjust_startup_res(tmp_file, log_name)
+    subprocess.run(f"docker rm -f {cid}", shell=True, stdout=subprocess.DEVNULL,
+                                        stderr=subprocess.STDOUT)
 
 def redis_ops(log_name, runtimes, tries=100000):
     rtime = ""
@@ -106,20 +111,47 @@ def _adjust_redis_res(files):
             row = data + v
             _writer.writerow(row)
 
-def _median(file):
+def _adjust_startup_res(src_fd, dest_file):
     data = {}
-    with open(file.as_posix(), 'r') as f:
-        content = f.readlines()
+    with open(src_fd, 'r') as fd:
+        content = fd.readlines()
+        print(content)
         runtime = ""
         times = int((content[0].split(','))[-1].split(':')[-1].replace('\n', ''))
         for i in range(0, len(content) - times, times + 2):
             head = content[i].split(',')
-            runtime = head[1].split(':')[-1]
+            runtime = head[0].split(':')[-1]
             sum = 0
             for j in range(0, times):
                 sum = sum + float(content[i+2+j].replace('\n', ''))
-            data[runtime] = sum / times
-    print(data)
+            data[runtime] = "%.9f" % (sum / times)
+        print(data)
+        with open(dest_file, 'a', newline='') as _csv:
+            header = ['test']
+            keys = data.keys()
+            for k in keys:
+                header.append(k)
+            row = ['startup']
+            for h in header[1:]:
+                row.append(data[h])
+            _writer = csv.writer(_csv)
+            _writer.writerow(header)
+            _writer.writerow(row)
+
+def _median(file):
+    data = {}
+#    with open(file.as_posix(), 'r') as f:
+#        content = f.readlines()
+#        runtime = ""
+#        times = int((content[0].split(','))[-1].split(':')[-1].replace('\n', ''))
+#        for i in range(0, len(content) - times, times + 2):
+#            head = content[i].split(',')
+#            runtime = head[1].split(':')[-1]
+#            sum = 0
+#            for j in range(0, times):
+#                sum = sum + float(content[i+2+j].replace('\n', ''))
+#            data[runtime] = sum / times
+#    print(data)
     return data
 
 def build_plot(file):
@@ -136,8 +168,11 @@ def build_plot(file):
     plt.show()
 
 def main():
-    _adjust_redis_res(['/tmp/redis-ops/09-04-2025_12-47-57.tmp.native.csv'])
-    return 0
+    #_adjust_redis_res(['/tmp/redis-ops/09-04-2025_12-47-57.tmp.native.csv'])
+    #return 0
+    #_adjust_startup_res('/tmp/startup/09-04-2025_16-20-28.csv.tmp',
+    #                    '/tmp/startup/09-04-2025_16-20-28.csv')
+    #return 0
     argpars = argparse.ArgumentParser()
     argpars.add_argument('--type', help='Performance measurement or plot collected data',
                          choices=['startup', 'redis-ops', 'plot'], default='startup')
